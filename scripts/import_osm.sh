f9da6e7cd68fe2b5e92b3ce4b84fd61e36836ad4
Riku Oja <riku.oja@kapsi.fi>
2021-06-22 17:48:40 +0300

    Use slim osm import to get primary key indexes in all tables

--- scripts/import_osm.sh
+++ scripts/import_osm.sh
@@ -1,3 +1,3 @@
 createdb geoviz
 curl http://download.openstreetmap.fr/extracts/europe/finland.osm.pbf > finland.osm.pbf
-osm2pgsql -d geoviz -O flex finland.osm.pbf -S ./flex-config/generic.lua
+osm2pgsql -d geoviz -O flex finland.osm.pbf -S ./flex-config/generic.lua --slim
