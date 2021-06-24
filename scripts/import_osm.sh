3c12a6a78d5cf36ba4a2c3fc637fdc95394f7829
Riku Oja <riku.oja@kapsi.fi>
2021-06-24 16:37:51 +0300

    Allow calling import_osm.sh outside scripts directory

--- scripts/import_osm.sh
+++ scripts/import_osm.sh
@@ -1,9 +1,10 @@
 # invoke with ./import_osm.sh europe finland
-
+SCRIPT=`realpath $0`
+SCRIPTPATH=$(dirname "$SCRIPT")
 createdb geoviz
 if curl --fail http://download.openstreetmap.fr/extracts/$1/$2.osm.pbf > $2.osm.pbf; then
     echo "OSM extract for $2 downloaded, importing to PostGIS..."
-    osm2pgsql -d geoviz -O flex $2.osm.pbf -S ./flex-config/generic.lua --slim
+    osm2pgsql -d geoviz -O flex $2.osm.pbf -S $SCRIPTPATH/flex-config/generic.lua --slim
 else
     echo "No OSM data found in $1 with country name $2"
     exit 1
