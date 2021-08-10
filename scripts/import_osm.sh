f513852f3cff6784fac7f3b47d51fa38411152bd
Riku Oja <riku.oja@kapsi.fi>
2021-08-10 16:12:38 +0300

    Use geofabrik and reuse downloaded osm extracts

--- scripts/import_osm.sh
+++ scripts/import_osm.sh
@@ -2,10 +2,12 @@
 SCRIPT=`realpath $0`
 SCRIPTPATH=$(dirname "$SCRIPT")
 createdb geoviz
-if curl --fail http://download.openstreetmap.fr/extracts/$1/$2.osm.pbf > $2.osm.pbf; then
+if test -f "$2.osm.pbf"; then
+    echo "OSM extract for $2 found cached, importing to PostGIS..."
+elif curl --fail http://download.geofabrik.de/$1/$2-latest.osm.pbf > $2.osm.pbf; then
     echo "OSM extract for $2 downloaded, importing to PostGIS..."
-    osm2pgsql -d geoviz -O flex $2.osm.pbf -S $SCRIPTPATH/flex-config/generic.lua --slim
 else
     echo "No OSM data found in $1 with country name $2"
     exit 1
 fi
+osm2pgsql -d geoviz -O flex $2.osm.pbf -S $SCRIPTPATH/flex-config/generic.lua --slim
