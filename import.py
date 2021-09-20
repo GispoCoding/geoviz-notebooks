a69d892819df95257e722a95a2f14a9df098b78e
Riku Oja <riku.oja@kapsi.fi>
2021-09-20 15:25:45 +0300

    Allow specifying larger bbox, to import multi-city regions with one command

--- import.py
+++ import.py
@@ -24,17 +24,19 @@ load_dotenv()
 osm_extracts_api_key = os.getenv("OSM_EXTRACTS_API_KEY")
 
 parser = argparse.ArgumentParser(description="Import all datasets for a given city")
-parser.add_argument("city", default="Helsinki", help="City data to import")
+parser.add_argument("city", default="Helsinki", help="City to import")
 parser.add_argument("--gtfs", help="Optional GTFS feed URL")
 parser.add_argument("--datasets",
                     default="osm flickr gtfs access ookla kontur",
-                    help="Datasets to import. Default is to import all."
+                    help="Datasets to import. Default is to import all. E.g. \"osm access kontur\""
                     )
+parser.add_argument("--bbox", help="Use different bbox for the city. Format \"minx miny maxx maxy\"")
 
 args = vars(parser.parse_args())
 city = args["city"]
 datasets = args["datasets"].split()
 gtfs_url = args.get("gtfs_url", None)
+bbox = args.get("bbox", None)
 print(f"--- Importing datasets {datasets} for {city} ---")
 
 # Get bbox, centroid and country for the city
@@ -42,7 +44,13 @@ city_params = {"q": args["city"], "limit": 1, "format": "json"}
 city_data = requests.get(
     "https://nominatim.openstreetmap.org/search", params=city_params
 ).json()[0]
-bbox = city_data["boundingbox"]
+if bbox:
+    bbox = bbox.split()
+else:
+    # nominatim returns miny, maxy, minx, maxx
+    # we want minx, miny, maxx, maxy
+    bbox = [city_data["boundingbox"][i] for i in [2, 0, 3, 1]]
+bbox = [float(coord) for coord in bbox]
 centroid = [city_data["lon"], city_data["lat"]]
 
 country_params = {
@@ -73,7 +81,7 @@ if "flickr" in datasets:
     print(f"--- Importing Flickr data for {city} ---")
     # flickr wants the bbox as (minx, miny, maxx, maxy) string for now
     flick_importer = FlickrImporter(
-        bbox=f"{float(bbox[2])}, {float(bbox[0])}, {float(bbox[3])}, {float(bbox[1])}"
+        bbox=",".join(bbox)
     )
     flick_importer.run()
 
@@ -90,9 +98,7 @@ if "gtfs" in datasets:
 if "access" in datasets:
     print(f"--- Importing OSM walkability & accessibility data for {city} ---")
     # osmnx wants the bbox as minx, miny, maxx, maxy floats for now
-    accessibility_importer = AccessibilityImporter(
-        float(bbox[2]), float(bbox[0]), float(bbox[3]), float(bbox[1])
-    )
+    accessibility_importer = AccessibilityImporter(*bbox)
     accessibility_importer.run()
 
 if "ookla" in datasets:
@@ -100,7 +106,7 @@ if "ookla" in datasets:
     # ookla importer wants the bbox as minx, miny, maxx, maxy floats for now
     ookla_importer = OoklaImporter(
         city=city,
-        bbox=(float(bbox[2]), float(bbox[0]), float(bbox[3]), float(bbox[1]))
+        bbox=bbox
     )
     ookla_importer.run()
 
@@ -109,7 +115,7 @@ if "kontur" in datasets:
     # kontur importer wants the bbox as minx, miny, maxx, maxy floats for now
     kontur_importer = KonturImporter(
         city=city,
-        bbox=(float(bbox[2]), float(bbox[0]), float(bbox[3]), float(bbox[1]))
+        bbox=bbox
     )
     kontur_importer.run()
 
