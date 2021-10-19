c18d6dd332000f88213f8093aef87fdda4baa1ca
Riku Oja <riku.oja@kapsi.fi>
2021-10-19 12:29:35 +0300

    Switch to Stamen tiles

--- server/static/map.js
+++ server/static/map.js
@@ -1,5 +1,6 @@
 var bbox_map = L.map('bbox_map').setView([60.5, 24.2], 8);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+
+L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     id: 'osm',
