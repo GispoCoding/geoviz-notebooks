eee5e3e29eb27ebc8bf6a7bb871ee3c47b1a5653
Riku Oja <riku.oja@kapsi.fi>
2021-10-20 12:48:40 +0300

    Add autocomplete to map

--- server/static/autocomplete.js
+++ server/static/autocomplete.js
@@ -2,5 +2,9 @@ var autocomplete = new kt.OsmNamesAutocomplete(
     'city_search', 'https://geoviz.gispocoding.fi/');
 
 autocomplete.registerCallback(function(item) {
-  alert(JSON.stringify(item, ' ', 5));
+    bbox = item.boundingbox
+    document.getElementById('bbox').value = bbox.join();
+    bounds = [[bbox[1],bbox[0]],[bbox[3],bbox[2]]];
+    L.rectangle(bounds, {color: "#ff7800", weight: 1}).addTo(bboxFeature);
+    bboxMap.fitBounds(bounds);
 });
--- server/static/map.js
+++ server/static/map.js
@@ -1,4 +1,4 @@
-var bbox_map = L.map('bbox_map').setView([60.5, 24.2], 8);
+var bboxMap = L.map('bbox_map').setView([60.5, 24.2], 8);
 
 L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}@2x.png', {
     maxZoom: 18,
@@ -6,4 +6,13 @@ L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}@2x.png', {
     id: 'osm',
     tileSize: 512,
     zoomOffset: -1
-}).addTo(bbox_map);
+}).addTo(bboxMap);
+var bboxFeature = new L.FeatureGroup();
+var drawControlEditOnly = new L.Control.Draw({
+    edit: {
+        featureGroup: bboxFeature
+    },
+    draw: false
+});
+bboxMap.addControl(drawControlEditOnly);
+bboxMap.addLayer(bboxFeature);
