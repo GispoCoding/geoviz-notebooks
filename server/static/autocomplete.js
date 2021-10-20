9832fff2d3bb16fd12c56ec99586c7e96d21fbfa
Riku Oja <riku.oja@kapsi.fi>
2021-10-20 13:38:28 +0300

    Put opt_forHilite in the right function

--- server/static/autocomplete.js
+++ server/static/autocomplete.js
@@ -1,10 +1,10 @@
 var autocomplete = new kt.OsmNamesAutocomplete(
     'city_search', 'https://geoviz.gispocoding.fi/');
 
-autocomplete.registerCallback(function(item, True) {
+autocomplete.registerCallback(function(item) {
     bbox = item.boundingbox
     document.getElementById('bbox').value = bbox.join();
     bounds = [[bbox[1],bbox[0]],[bbox[3],bbox[2]]];
     L.rectangle(bounds, {color: "#ff7800", weight: 1}).addTo(bboxFeature);
     bboxMap.fitBounds(bounds);
-});
+}, true);
