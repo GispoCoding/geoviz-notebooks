ed4379d4c9d47a51921b9da9fcafcf2092aa5f8b
Riku Oja <riku.oja@kapsi.fi>
2021-10-21 16:34:50 +0300

    Add WIP analysis db table

--- models.py
+++ models.py
@@ -1,9 +1,31 @@
-from sqlalchemy import Column, BigInteger, String
+import datetime
+from sqlalchemy import Column, BigInteger, Boolean, DateTime, Integer, String
+from sqlalchemy.sql import expression, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.postgresql import JSONB
 from geoalchemy2 import Geometry
 
 Base = declarative_base()
+
+
+# Here we store the current and past import runs
+class Analysis(Base):
+    __tablename__ = 'analyses'
+    id = Column(Integer, primary_key=True)
+    slug = Column(String, unique=True, nullable=False)
+    # By default, populate the slug from the city name. Still, future proof
+    # the DB just in case we start having multiple analyses with multiple
+    # slugs for the same city, or analyses/cities with identical names.
+    name = Column(String, nullable=False)
+    bbox = Column(Geometry(geometry_type='POLYGON'))
+    # Future-proof the datasets field too. We might get more datasets, we
+    # don't want to have to do migrations to the analysis table in that case.
+    datasets = Column(JSONB)
+    start_time = Column(DateTime, nullable=False, default=datetime.datetime.now, server_default=func.now())
+    finish_time = Column(DateTime)
+    # set this field True once the user has seen the result
+    viewed = Column(Boolean, nullable=False, default=False, server_default=expression.false())
+
 
 # Define the desired tables as below, or use sqlalchemy autofind to
 # automatically define the classes based on the imported OSM tables?
--- server/serve.py
+++ server/serve.py
@@ -1,12 +1,20 @@
 import os
 import secrets
+import sys
 from dotenv import load_dotenv
 from flask import Flask, render_template, send_from_directory
 from flask_httpauth import HTTPBasicAuth
+from ipygis import get_connection_url
+from slugify import slugify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from werkzeug.security import check_password_hash
 
 from forms import AnalysisForm
 
+# test simple import now, convert to module later
+sys.path.insert(0, "..")
+from models import Analysis
 
 app = Flask(__name__, static_url_path='')
 auth = HTTPBasicAuth()
@@ -17,6 +25,11 @@ secret_key = os.getenv("SECRET_KEY")
 if not secret_key:
     secret_key = secrets.token_bytes(64)
 app.config['SECRET_KEY'] = secret_key
+
+sql_url = get_connection_url(dbname="geoviz")
+engine = create_engine(sql_url)
+session = sessionmaker(bind=engine)()
+Analysis.__table__.create(engine, checkfirst=True)
 
 
 @auth.verify_password
@@ -45,12 +58,21 @@ def home():
     form = AnalysisForm()
     if form.validate_on_submit():
         running = True
+        print(form.bbox.data)
+        city_name = form.bbox.data
+        # analysis = Analysis(
+        #     slug = slugify(form.)
+        # )
+    unfinished_analyses = session.query(Analysis).filter(Analysis.viewed.is_(False)).all()
+    if unfinished_analyses:
+        running = True
     return render_template(
         'home.html',
         title="Gispo Spatial Analytics",
         description="Import urban datasets and run analyses.",
         form=form,
-        running=running
+        running=running,
+        analyses=unfinished_analyses
     )
 
 
