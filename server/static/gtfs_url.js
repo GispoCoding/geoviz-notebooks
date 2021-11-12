40ebec885610cb8502793d82669887b6c3bed322
Riku Oja <riku.oja@kapsi.fi>
2021-11-12 18:49:55 +0200

    Handle errors properly at gtfs_url

--- server/static/gtfs_url.js
+++ server/static/gtfs_url.js
@@ -1,10 +1,17 @@
 function setGtfsUrl(city) {
     url = '/gtfs_url/' + city;
     fetch(url)
-        .then(response => response.json())
+        .then(response => {
+            if (response.status == 404) {
+                return null
+            }
+            return response.json()
+        })
         .then(data => {
             if (data) {
                 document.getElementById('gtfs_url').value = data.url;
+            } else {
+                document.getElementById('gtfs_url').value = '';
             };
         });
 };
