1f905b00d419e2102a555da410e29bab70f4b4ac
Riku Oja <riku.oja@kapsi.fi>
2021-11-12 18:49:26 +0200

    Allow loading js files on subpages

--- server/static/autocomplete.js
+++ server/static/autocomplete.js
@@ -1,7 +1,9 @@
-var autocomplete = new kt.OsmNamesAutocomplete(
-    'bbox-city', osmnamesUrl);
+if (document.getElementById('bbox-city')) {
+    var autocomplete = new kt.OsmNamesAutocomplete(
+        'bbox-city', osmnamesUrl);
 
-autocomplete.registerCallback(function (item) {
-    setBbox(item.boundingbox);
-    setGtfsUrl(item.name);
-}, true);
+    autocomplete.registerCallback(function (item) {
+        setBbox(item.boundingbox);
+        setGtfsUrl(item.name);
+    }, true);
+};
